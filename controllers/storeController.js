040363596fadd16c6f63a7475af7dd2bd65ddd43
Jason Vance <jbvance@gmail.com>
2018-01-01 12:32:00 -0600

    complete add new store functionality

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,8 @@
+const mongoose = require('mongoose');
+// You can reference 'Store' model here directly because it was already imported
+// in start.js.
+const Store = mongoose.model('Store')
+
 exports.homePage = (req, res) => {
     res.render('index');
 }
@@ -6,6 +11,8 @@ exports.addStore = (req, res) => {
     res.render('editStore', {title: 'Add Store'});
 };
 
-exports.createStore = (req, res) => {
-    res.json(req.body)
+exports.createStore = async (req, res) => {
+    const store = new Store(req.body);
+    await store.save();
+    res.redirect('/');
 };
