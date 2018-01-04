440c9a661e8465883441984e60e037fdbcda7df8
Jason Vance <jbvance@gmail.com>
2018-01-03 20:33:40 -0600

    add view store page

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,9 @@ router.post('/add/:id',
     storeController.upload, 
     catchErrors(storeController.resize),
     catchErrors(storeController.updateStore));
-    
+
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
+
+router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
 module.exports = router;
