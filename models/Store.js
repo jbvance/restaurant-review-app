ff061b7b8dc36f8b67db665a6a8162dac84a44b0
Jason Vance <jbvance@gmail.com>
2018-01-04 21:13:07 -0600

    add tags aggregration and view

--- models/Store.js
+++ models/Store.js
@@ -52,4 +52,12 @@ storeSchema.pre('save', async function(next) {
     //TODO make sure slug is unique    
 })
 
+storeSchema.statics.getTagsList = function() {
+    return this.aggregate([
+        { $unwind: '$tags' },
+        { $group: { _id: '$tags', count: { $sum: 1 } }},
+        { $sort: { count: -1  }}
+    ]);
+};
+
 module.exports = mongoose.model('Store', storeSchema);
